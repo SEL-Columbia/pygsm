428b00ff47d2168e0c2abad64c2a9e6081fe2c26
Ivan Willig <iwillig@gmail.com>
2010-11-03 20:52:05 -0400

    Moved some stuff around so we can use easy install. Ivan is happy now.

--- setup.py
+++ setup.py
@@ -1,76 +1,32 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+from setuptools import setup, find_packages
+import sys, os
 
-# PyGSM setup.py -- adapted from Django's with our heartfelt thanks
+version = '.1'
 
-from __future__ import with_statement
-
-from distutils.core import setup
-from distutils.command.build_py import build_py as _build_py
-from distutils.command.install import INSTALL_SCHEMES
-import os
-import sys
-import commands
-import traceback
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-wd = os.getcwd()
-os.chdir('lib')
-packages = [ 
-    dir
-    for dir,dirs,files
-    in os.walk('pygsm')
-    if '__init__.py' in files
-    ]
-os.chdir(wd)
-
-# Dynamically calculate the version based on get_rapidsms_version()
-# ... this way releases automagically get shipped with the version
-# stored in the git tag, and installations (whether from releases or not)
-# are tagged with a reasonably exact git version.
-#
-sys.path = ["lib"] + sys.path
-version = __import__("pygsm").get_version()
-
-class build_py (_build_py):
-    def run (self):
-        _build_py.run(self)
-        if version == "unknown":
-            print "PyGSM version unknown! Is git in your path?"
-        else:
-            vstring = "VERSION = '%s'" % version 
-            vfilename = os.path.join(self.build_lib,
-                        "pygsm", "__version__.py")
-            try:
-                with open(vfilename, 'w') as f:
-                    f.write(vstring)
-            except:
-                traceback.print_exc()
-            print "setting %s in %s" % (vstring, vfilename)
-
-setup(
-    name = "pygsm",
-    version = version,
-    maintainer = "RapidSMS development community",
-    maintainer_email = "rapidsms@googlegroups.com",
-    description = "Library for interfacing with GSM modems",
-    url = "http://www.rapidsms.org/",
-    package_dir = {'': 'lib'},
-    packages = packages,
-    # data_files = data_files,
-    # package_data = {'rapidsms': ['skeleton/project/*.ini',
-    #                             'skeleton/project/manage.py']},
-    scripts = ['pygsm_demo'],
-    cmdclass={'build_py': build_py},
-    long_description = """
+setup(name='pygsm',
+      version=version,
+      description="",
+      long_description="""\
 PyGSM is a Free and Open Source library for interfacing with
 GSM modems and handsets to send and receive SMS messages.
-"""
-)
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+        "pyserial",
+        "pytz",
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
